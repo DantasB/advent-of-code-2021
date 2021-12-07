06786262a305d6514a41cf879effdbdc905e3d39
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-12-06 21:16:47 -0300

    Add first puzzle solution

---
+++ Day01/FirstPuzzle/go.mod
@@ -0,0 +1,3 @@
+module github.com/DantasB/advent-of-code-2021/Day01/FirstPuzzle
+
+go 1.17
---
+++ Day01/FirstPuzzle/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func pulseIncreased(previous int, current int) bool {
+	return previous < current
+}
+
+func convertToInteger(line string) int {
+	trimmedLine := strings.TrimSuffix(line, "\n")
+	converted, err := strconv.Atoi(trimmedLine)
+	if err != nil {
+		fmt.Printf("[ERROR] Can't convert the value %v to integer", trimmedLine)
+		panic(err)
+	}
+
+	return converted
+}
+
+func readFile(fileName string) *os.File {
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Printf("[ERROR] Can't read the file %v", fileName)
+		panic(err)
+	}
+
+	return file
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("[ERROR] Missing parameter. You must specify a file path.")
+		return
+	}
+
+	file := readFile(os.Args[1])
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanLines)
+
+	previousMeasurement, currentMeasurement, result := 0, 0, 0
+	for scanner.Scan() {
+		measure := scanner.Text()
+		if previousMeasurement == 0 {
+			previousMeasurement = convertToInteger(measure)
+			continue
+		}
+
+		currentMeasurement = convertToInteger(measure)
+		if pulseIncreased(previousMeasurement, currentMeasurement) {
+			result++
+		}
+
+		previousMeasurement = currentMeasurement
+	}
+
+	fmt.Printf("[INFO] The obtained result is: %v", result)
+}
